690e2e9e0b92c95691686e3485dbd318febd9a41
Mariano Chirino <marianochirino95@gmail.com>
2026-05-14 22:18:52 -0300

    fix cositas
    detalles superficiales.

--- ejercicios-genericos-java/src/main/java/src/Main.java
+++ ejercicios-genericos-java/src/main/java/src/Main.java
@@ -1,9 +1,5 @@
 package src;
 
-/**
- *
- * @author Mariano_Chirino
- */
 public class Main {
 
     public static void main(String[] args) {
--- ejercicios-genericos-java/src/main/java/src/par/Par.java
+++ ejercicios-genericos-java/src/main/java/src/par/Par.java
@@ -5,5 +5,13 @@ package src.par;
  * @author Mariano_Chirino
  */
 public class Par {
-
+    /*
+    Ejercicio 3 — Par<A,B>
+    Crear una clase genérica que almacene dos valores.
+    Agregar un método:
+    invertir()
+    que devuelva el par invertido.
+     */
+    
+    
 }
