c308e03dab6a41870ff4d3c2bb4e24ea890c75cf
andresfabre27 <andres_fabre@hotmail.com>
2026-05-17 07:17:56 -0300

    Actualización Ejercicio Nro 4
    Actualización Ejercicio Nro 4

--- ejercicios-genericos-java/src/main/java/src/Main.java
+++ ejercicios-genericos-java/src/main/java/src/Main.java
@@ -18,6 +18,7 @@ public class Main {
         // Ahora el resultado es un Par de String (B) e Integer (A) 
         Par<String, Integer> pInvertido = p.invertir();
         System.out.println("Invertido: " + pInvertido);
+        
         //Probamos calculadora
         //Creamos las listas de numeros
         List<Integer> lista_enteros = Arrays.asList(2, 8, 5, 10);
@@ -26,10 +27,11 @@ public class Main {
         //Imprimimos las listas
         System.out.println("Lisa de enteros: "+lista_enteros);
         System.out.println("Lista de decimales: "+lista_decimales);
-        //Utilizamos los metodos
+        //Utilizamos los metodos genericos de la clase calculadora
+        //metodos maximo y promedio para la lista de enteros
         System.out.println("Promedio lisa de enteros: "+Calculadora.promedio(lista_enteros));
         System.out.println("Maximo lista de enteros: "+Calculadora.maximo(lista_enteros));
-
+        //metodos maxiumo y promedio para la lista de decimales
         System.out.println("Promedio lista de decimales: "+Calculadora.promedio(lista_decimales));
         System.out.println("Maximo lista de decimales: "+Calculadora.maximo(lista_decimales));
     }
